f5ef390ec281a141e04444dca071e16d028f5340
David Hurley <dhurls99@gmail.com>
2023-04-18 04:54:20 -0400

    remove contract file

--- lib/contract.ts
+++
@@ -1,7 +0,0 @@
-export type AbiFunctionReturnType<
-  TAbi extends Abi,
-  TFunctionName extends string
-> = IsContractsFileMissing<
-  any,
-  AbiParametersToPrimitiveTypes<AbiFunctionOutputs<TAbi, TFunctionName>>[0]
->;
