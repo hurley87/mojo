52d765b4e2bdb0122d7be6925637a8b9b06a1375
David Hurley <dhurls99@gmail.com>
2023-05-05 19:04:03 -0400

    add odds for hockey

--- pages/hockey.tsx
+++ pages/hockey.tsx
@@ -4,9 +4,16 @@ import Layout from '@/components/Layout';
 export default function Hockey() {
   return (
     <Layout>
-      <div className="lg:pt-10 w-full">
-        <Games />
-      </div>
+      <p className="text-sm pt-2 pb-3">
+        <a
+          target="_blank"
+          className="text-primary underline"
+          href="https://thescore.bet"
+        >
+          Read more about the NHL playoffs and the odds for each game.
+        </a>
+      </p>
+      <Games />
     </Layout>
   );
 }
