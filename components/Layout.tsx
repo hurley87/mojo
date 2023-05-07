752dab6fd28cf5ba1d3cea800259f7d4668ddf03
David Hurley <dhurls99@gmail.com>
2023-05-07 07:33:01 -0400

    update  title

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,21 +12,21 @@ const Layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col h-screen justify-between p-4">
       <Head>
-        <title>MOJO</title>
+        <title>Mojo | P2P Sports Betting</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="P2P sports betting" />
+        <meta name="description" content="P2P Sports Betting" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@davidhurley87" />
-        <meta name="twitter:title" content="MOJO" />
-        <meta name="twitter:description" content="P2P sports betting" />
+        <meta name="twitter:title" content="Mojo" />
+        <meta name="twitter:description" content="P2P Sports Betting" />
         <meta
           name="twitter:image"
           content="https://pollock-art.s3.amazonaws.com/mojo.png"
         />
         <meta property="og:url" content={`https://mojo.club`} />
-        <meta property="og:title" content="MOJO" />
-        <meta property="og:description" content="P2P sports betting" />
+        <meta property="og:title" content="Mojo" />
+        <meta property="og:description" content="P2P Sports Betting" />
         <meta
           property="og:image"
           content="https://pollock-art.s3.amazonaws.com/mojo.png"
@@ -39,7 +39,7 @@ const Layout = ({ children }: Props) => {
         </div>
       </div>
       <footer className="footer text-base-content footer-center">
-        <p>Copyright © 2023 - All right reserved by MOJO</p>
+        <p>Copyright © 2023 - All right reserved by Mojo</p>
       </footer>
     </div>
   );
