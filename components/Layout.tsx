286f6a781a0f5507d5e5d2d2dd31461aa561529b
David Hurley <dhurls99@gmail.com>
2023-05-07 14:24:48 -0400

    update meta data

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -18,14 +18,14 @@ const Layout = ({ children }: Props) => {
         <meta name="description" content="P2P Sports Betting" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@davidhurley87" />
-        <meta name="twitter:title" content="Mojo" />
+        <meta name="twitter:title" content="Mojo | P2P Sports Betting" />
         <meta name="twitter:description" content="P2P Sports Betting" />
         <meta
           name="twitter:image"
           content="https://pollock-art.s3.amazonaws.com/mojo.png"
         />
         <meta property="og:url" content={`https://mojo.club`} />
-        <meta property="og:title" content="Mojo" />
+        <meta property="og:title" content="Mojo | P2P Sports Betting" />
         <meta property="og:description" content="P2P Sports Betting" />
         <meta
           property="og:image"
