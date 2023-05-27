8d06948bf867891b7e96d3f50668c6e713302d18
David Hurley <dhurls99@gmail.com>
2023-05-27 18:19:30 -0400

    handle approval for accept feature

--- components/BetAccept.tsx
+++ components/BetAccept.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import useBetsWrite from '@/hooks/useBetsWrite';
 import { useBetsSubscriber } from '@/hooks/useBetsSubscribe';
 import { BetAccepted } from './BetAccepted';
@@ -11,11 +11,15 @@ import { useTeamsRead } from '@/hooks/useTeamsRead';
 import { UserContext } from '@/lib/UserContext';
 import { makeBig, makeNum } from '@/lib/number-utils';
 import { useMojoRead } from '@/hooks/useMojoRead';
+import { useMojoSubscriber } from '@/hooks/useMojoSubscribe';
+import useMojoWrite from '@/hooks/useMojoWrite';
 
 export const BetAccept = ({ betId, sport }: { betId: string; sport: any }) => {
   const [user, _]: any = useContext(UserContext);
   const [isLoading, setIsLoading] = useState(false);
   const [hasAccepted, setHasAccepted] = useState(false);
+  const [isApproveLoading, setIsApproveLoading] = useState(false);
+  const [isApproved, setIsApproved] = useState(false);
   const betsContract = useBetsWrite(sport.betsAddress);
   const { data: bet } = useBetsRead({
     address: sport.betsAddress,
@@ -46,7 +50,53 @@ export const BetAccept = ({ betId, sport }: { betId: string; sport: any }) => {
     functionName: 'balanceOf',
     args: [user?.publicAddress],
   });
+  const { data: mojoAllowance } = useMojoRead({
+    functionName: 'allowance',
+    args: [user?.publicAddress, sport.betsAddress],
+  });
+  const mojoContract = useMojoWrite();
 
+  // is mojoAllowance > bet.counter and set isApproved to true using useEffect
+  useEffect(() => {
+    if (
+      mojoAllowance &&
+      bet &&
+      parseInt(makeNum(mojoAllowance)) >= bet?.counter.toNumber()
+    ) {
+      setIsApproved(true);
+    }
+  }, [mojoAllowance, bet]);
+
+  async function handleApprove() {
+    setIsApproveLoading(true);
+    try {
+      await mojoContract?.approve(sport.betsAddress, mojoBalance);
+    } catch (e) {
+      toast.error('try again');
+      setIsApproveLoading(false);
+      va.track('TokensApproveError', {
+        address: user?.publicAddress,
+      });
+      return;
+    }
+  }
+
+  useMojoSubscriber({
+    eventName: 'Approval',
+    listener: (walletAddress: string, spender: string, amount: any) => {
+      console.log(spender, walletAddress, amount);
+      if (
+        walletAddress.toLocaleLowerCase() ===
+        user?.publicAddress?.toLocaleLowerCase()
+      ) {
+        setIsApproveLoading(false);
+        setIsApproved(true);
+        va.track('TokensClaims', {
+          address: walletAddress,
+        });
+      }
+    },
+  });
   useBetsSubscriber({
     address: sport.betsAddress,
     eventName: 'BetAccepted',
@@ -86,16 +136,35 @@ export const BetAccept = ({ betId, sport }: { betId: string; sport: any }) => {
         <button disabled={true} className={`btn`}>
           You need more tokens to place this bet
         </button>
+      ) : isApproved ? (
+        <button
+          onClick={handleApprove}
+          disabled={isApproveLoading}
+          className={`btn btn-primary ${
+            isApproveLoading
+              ? 'loading before:!w-4 before:!h-4 before:!mx-0 before:!mr-1'
+              : ''
+          }`}
+        >
+          {isApproveLoading
+            ? 'Approving ...'
+            : `Approve ${bet?.counter.toNumber()} MOJO`}
+        </button>
       ) : (
         <button
           onClick={handleAcceptBet}
+          disabled={isLoading}
           className={`btn btn-primary ${
             isLoading
               ? 'loading before:!w-4 before:!h-4 before:!mx-0 before:!mr-1'
               : ''
           }`}
         >
-          Place {bet?.counter.toNumber()} MOJO on the {otherTeamPicked?.name}
+          {isLoading
+            ? 'Confirming ...'
+            : `Confirm ${bet?.counter.toNumber()} MOJO bet on the ${
+                otherTeamPicked?.name
+              }`}
         </button>
       )}
     </>
